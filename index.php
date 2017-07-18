e87c9a99c3394105113715346bf19c07c5a178fd
Orde Saunders <orde@decadecity.net>
2017-07-18 23:57:34 +0100

    Brute force removal of featured items.

--- index.php
+++ index.php
@@ -44,9 +44,10 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-				?><hr class="post-separator"/><?php
+				if ( !has_tag( 'featured' ) ) : // TODO: Find a better way to do this.
+					get_template_part( 'template-parts/content', get_post_format() );
+					?><hr class="post-separator"/><?php
+				endif;
 
 			endwhile;
 
