653d6e37e59bdbe635c1216aff1765aaccff8e47
Orde Saunders <orde@decadecity.net>
2017-09-07 16:07:23 +0100

    Cleanup for BoB diary prod issues.

--- functions.php
+++ functions.php
@@ -229,3 +229,12 @@ remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+// Enable HTML in category/tag desriptions.
+foreach ( array( 'pre_term_description' ) as $filter ) {
+	remove_filter( $filter, 'wp_filter_kses' );
+}
+
+foreach ( array( 'term_description' ) as $filter ) {
+	remove_filter( $filter, 'wp_kses_data' );
+}
