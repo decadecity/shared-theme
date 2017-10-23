a0f94c541b75a0a2a6cd872c98884e0cd033f66e
Orde Saunders <orde@decadecity.net>
2017-10-23 17:05:58 +0100

    Updates meta.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -12,6 +12,12 @@ if ( ! function_exists( 'shared_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function shared_posted_on() {
+	if (is_bob_diary()) {
+		$posted = 'Date: ';
+	} else {
+		$posted = 'Posted on';
+	}
+
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -26,17 +32,11 @@ function shared_posted_on() {
 
 	$posted_on = sprintf(
 		/* translators: %s: post date. */
-		esc_html_x( 'Posted on %s', 'post date', 'shared' ),
+		esc_html_x( $posted . ' %s', 'post date', 'shared' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		/* translators: %s: post author. */
-		esc_html_x( 'by %s', 'post author', 'shared' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
